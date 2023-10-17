09789b0553dd3a53f93a35a7a5dd23dc36b97410
IBRAHIM102005 <ibrahimmmdov5@gmail.com>
2023-10-17 13:03:18 +0400

    dovrler

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Immutable;
+
+namespace massiv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("massivin ölçüsünü daxil edin--");
+            //int a=int.Parse(Console.ReadLine());
+            //int[] ededler =new int[a];
+ 
+            //int s = 0;
+
+            //for (int i = 0; i < ededler.Length; i++)
+            //{
+            //    ededler[i] = int.Parse(Console.ReadLine());
+            //    s=s+ededler[i];
+            //}
+
+            //Console.WriteLine(s);
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+            //massivin en böyük elementini tapır
+            Console.Write("massivin ölçüsünü daxil edin--");
+            int a=int.Parse(Console.ReadLine());
+
+            int [] ededler =new int[a];
+            
+            for (int i = 0; i < ededler.Length; i++)
+            {
+                ededler[i] = int.Parse(Console.ReadLine());
+               
+            }
+ 
+            int b = ededler[0];
+ 
+            
+            for (int i = 0; i < ededler.Length; i++)
+            {
+                
+                if (b <= ededler[i])
+                {
+                    b = ededler[i];
+                }
+            }
+            Console.WriteLine(b);
+            
+
+        }
+    }
+}
